2e1a93b1bc534b9cbe94c3a526e65bf02ec59681
Your Name <monarul007@gmail.com>
2020-12-29 18:20:38 +0600

    syntax error on app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', '{{$GenSettings->site_name}}') }}</title>
+    <title>{{ config('app.name', '{{$GenSettings->site_name)}}') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
